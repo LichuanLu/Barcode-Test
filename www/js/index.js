61838986c50f36114243f959ec26c517e280a9e5
LichuanLu <lulichuan628@gmail.com>
2013-02-04 18:01:02 +0800

    first commit

---
+++ www/js/index.js
@@ -0,0 +1,22 @@
+var app = {
+    initialize: function() {
+        this.bind();
+    },
+    bind: function() {
+        document.addEventListener('deviceready', this.deviceready, false);
+    },
+    deviceready: function() {
+        alert("ready");
+        window.plugins.barcodeScanner.scan(function(result) {
+            alert("We got a barcode\n" +
+                  "Result: " + result.text + "\n" +
+                  "Format: " + result.format + "\n" +
+                  "Cancelled: " + result.cancelled);
+        }, function(error) {
+                alert("Scanning failed: " + error);
+            }
+        );
+    }
+};
+
+
